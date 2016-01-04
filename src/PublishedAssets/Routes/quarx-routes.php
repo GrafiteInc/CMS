c736513709e4fd5ac5a3dcb6a5fecc35c97979f7
mlantz <mattlantz@gmail.com>
2016-01-04 02:01:50 -0500

    Minor correction

--- src/PublishedAssets/Routes/quarx-routes.php
+++ src/PublishedAssets/Routes/quarx-routes.php
@@ -6,7 +6,7 @@
     |--------------------------------------------------------------------------
     */
 
-    Route::group(['namespace' => 'App\Http\Controllers\Quarx', 'middleware' => ['web', 'guest']], function() {
+    Route::group(['namespace' => 'App\Http\Controllers\Quarx', 'middleware' => ['web']], function() {
 
         Route::get('', 'PagesController@home');
         Route::get('page', 'PagesController@all');

724e54dc388197c1a4b5bcc9378e208ff5f0aedd
mlantz <mattlantz@gmail.com>
2016-01-31 04:09:13 -0500

    code style

--- src/Views/modules/blogs/create.blade.php
+++ src/Views/modules/blogs/create.blade.php
@@ -2,21 +2,21 @@
 
 @section('content')
 
-        <div class="row">
-            <h1 class="page-header">Blog</h1>
+    <div class="row">
+        <h1 class="page-header">Blog</h1>
+    </div>
+
+    @include('quarx::modules.blogs.breadcrumbs', ['location' => ['create']])
+
+    {!! Form::open(['route' => 'quarx.blog.store']) !!}
+
+        {!! FormMaker::fromTable('blogs', Config::get('quarx.forms.blog')) !!}
+
+        <div class="form-group text-right">
+            <a href="{!! URL::to('quarx/blog') !!}" class="btn btn-default raw-left">Cancel</a>
+            {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
         </div>
 
-        @include('quarx::modules.blogs.breadcrumbs', ['location' => ['create']])
-
-        {!! Form::open(['route' => 'quarx.blog.store']) !!}
-
-            {!! FormMaker::fromTable('blogs', Config::get('quarx.forms.blog')) !!}
-
-            <div class="form-group text-right">
-                <a href="{!! URL::to('quarx/blog') !!}" class="btn btn-default raw-left">Cancel</a>
-                {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-            </div>
-
-        {!! Form::close() !!}
+    {!! Form::close() !!}
 
 @endsection

9c0c7c0bb2076fe0394d26984c39b943b0586cb0
mlantz <mattlantz@gmail.com>
2016-02-04 11:29:22 -0500

    Minor clean up

--- src/PublishedAssets/Prepare/resources/views/partials/navigation.blade.php
+++ src/PublishedAssets/Prepare/resources/views/partials/navigation.blade.php
@@ -3,11 +3,11 @@
     <div class="btn-group" role="group" aria-label="...">
         <a class="btn btn-default" href="/dashboard">Site Dashboard</a>
         @if (Auth::user()->can('quarx'))
-        <a class="btn btn-default" href="/quarx/dashboard">Quarx CMS</a>
+            <a class="btn btn-default" href="/quarx/dashboard">Quarx CMS</a>
         @endif
         <a class="btn btn-default" href="/account/settings">Account Settings</a>
         @if (Auth::user()->can('admin'))
-        <a class="btn btn-default" href="/admin/accounts">Account Admin</a>
+            <a class="btn btn-default" href="/admin/accounts">Account Admin</a>
         @endif
         <a class="btn btn-default" href="/teams">Team Manager</a>
     </div>

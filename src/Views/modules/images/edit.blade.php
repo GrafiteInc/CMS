838981f53270db360463b79cfc55e2be2ff631cc
mlantz <mattlantz@gmail.com>
2016-07-03 22:14:38 -0400

    minor style fix

--- src/Views/modules/images/edit.blade.php
+++ src/Views/modules/images/edit.blade.php
@@ -1,29 +1,28 @@
 @extends('quarx::layouts.dashboard')
 
 @section('content')
+    <div class="row">
+        <h1 class="page-header">Images</h1>
+    </div>
 
-        <div class="row">
-            <h1 class="page-header">Images</h1>
-        </div>
+    @include('quarx::modules.images.breadcrumbs', ['location' => ['edit']])
 
-        @include('quarx::modules.images.breadcrumbs', ['location' => ['edit']])
-
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="thumbnail ">
-                    <img src="{!! FileService::filePreview($images->location) !!}" />
-                </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="thumbnail ">
+                <img src="{!! FileService::filePreview($images->location) !!}" />
             </div>
         </div>
+    </div>
 
-        {!! Form::model($images, ['route' => ['quarx.images.update', $images->id], 'method' => 'patch', 'files' => true, 'class' => 'edit']) !!}
+    {!! Form::model($images, ['route' => ['quarx.images.update', $images->id], 'method' => 'patch', 'files' => true, 'class' => 'edit']) !!}
 
-            {!! FormMaker::fromObject($images, Config::get('quarx.forms.images-edit')) !!}
+        {!! FormMaker::fromObject($images, Config::get('quarx.forms.images-edit')) !!}
 
-            <div class="form-group text-right">
-                <a href="{!! URL::to('quarx/images') !!}" class="btn btn-default raw-left">Cancel</a>
-                {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-            </div>
+        <div class="form-group text-right">
+            <a href="{!! URL::to('quarx/images') !!}" class="btn btn-default raw-left">Cancel</a>
+            {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+        </div>
 
-        {!! Form::close() !!}
+    {!! Form::close() !!}
 @endsection

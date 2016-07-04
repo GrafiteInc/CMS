04d1e9efd30b2801ef46e427746196b5d7598f8c
mlantz <mattlantz@gmail.com>
2016-07-03 20:05:57 -0400

    Minor fix

--- src/Providers/QuarxModuleProvider.php
+++ src/Providers/QuarxModuleProvider.php
@@ -12,6 +12,19 @@ class QuarxModuleProvider extends ServiceProvider
     public function boot(DispatcherContract $events)
     {
         parent::boot($events);
+
+        $modulePath = base_path(Config::get('quarx.module-directory').'/');
+        $modules = glob($modulePath.'*');
+
+        if (is_array($modules)) {
+            while (list(, $module) = each($modules)) {
+                $module = str_replace($modulePath, '', $module);
+                if (!is_file(base_path(Config::get('quarx.module-directory').'/'.ucfirst($module).'/'.ucfirst($module).'ModuleProvider.php'))) {
+                    // Load events
+                    $events->listen('eloquent.saved: Quarx\Modules\\'.str_plural($module).'\\Models\\'.str_singular($module), 'Quarx\Modules\\'.str_plural($module).'\\Models\\'.str_singular($module).'@afterSaved');
+                }
+            }
+        }
     }
 
     public function register()
@@ -34,9 +47,6 @@ class QuarxModuleProvider extends ServiceProvider
                     if (file_exists($modulePath.$module.'/config.php')) {
                         Config::set('quarx.modules.'.strtolower($module), include($modulePath.$module.'/config.php'));
                     }
-
-                    // Load events
-                    $events->listen('eloquent.saved: Quarx\Modules\\'.str_plural($module).'\\Models\\'.str_singular($module), 'Quarx\Modules\\'.str_plural($module).'\\Models\\'.str_singular($module).'@afterSaved');
 
                     // Load the Views
                     if (is_dir($modulePath.$module.'/Views')) {

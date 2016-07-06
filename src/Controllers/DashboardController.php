738140a92823d7312dadc4f41c6a651369ced4e7
mlantz <mattlantz@gmail.com>
2016-07-06 12:03:18 -0400

    Merge branch 'master' of https://github.com/YABhq/Quarx into develop

--- src/Controllers/DashboardController.php
+++ src/Controllers/DashboardController.php
@@ -2,22 +2,25 @@
 
 namespace Yab\Quarx\Controllers;
 
-use Spatie\LaravelAnalytics\LaravelAnalyticsFacade as LaravelAnalytics;
+use Analytics;
+use Spatie\Analytics\Period;
 
 class DashboardController extends QuarxController
 {
     public function main()
     {
-        if (is_null(env('ANALYTICS_SITE_ID'))) {
+        if (is_null(env('ANALYTICS_VIEW_ID'))) {
             return view('quarx::dashboard.empty');
         }
 
-        foreach (LaravelAnalytics::getVisitorsAndPageViews(7) as $view) {
+        foreach (Analytics::fetchVisitorsAndPageViews(Period::days(7)) as $view) {
             $visitStats['date'][] = $view['date']->format('Y-m-d');
             $visitStats['visitors'][] = $view['visitors'];
             $visitStats['pageViews'][] = $view['pageViews'];
         }
 
-        return view('quarx::dashboard.analytics', ['visitStats' => $visitStats]);
+        $oneYear = Period::days(365);
+
+        return view('quarx::dashboard.analytics', compact('visitStats', 'oneYear'));
     }
 }

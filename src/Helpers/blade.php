a692e9f84a9ebd9cf5762ad4101901d94beef0bc
mlantz <mattlantz@gmail.com>
2016-07-05 20:55:39 -0400

    Helper fixes

--- src/Helpers/blade.php
+++ src/Helpers/blade.php
@@ -3,27 +3,27 @@
 if (!function_exists('menu')) {
     function menu($slug, $view = null)
     {
-        return app('Quarx')->menu($slug, $view);
+        return app('QuarxService')->menu($slug, $view);
     }
 }
 
 if (!function_exists('images')) {
     function images($tag = null)
     {
-        return app('Quarx')->images($tag);
+        return app('QuarxService')->images($tag);
     }
 }
 
 if (!function_exists('widget')) {
     function widget($slug)
     {
-        return app('Quarx')->widget($slug);
+        return app('QuarxService')->widget($slug);
     }
 }
 
 if (!function_exists('editBtn')) {
     function edit($module, $id = null)
     {
-        return app('Quarx')->module($module, $id);
+        return app('QuarxService')->module($module, $id);
     }
 }

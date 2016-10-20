bb5216fefe782492f946c55e955a1bc308ec1aa5
Matt Lantz <mattlantz@gmail.com>
2016-10-20 13:31:14 -0400

    Test fix

--- tests/ModuleMakeTest.php
+++ tests/ModuleMakeTest.php
@@ -12,41 +12,41 @@ class CrudSingleGeneratorTest extends PHPUnit_Framework_TestCase
     {
         $this->generator = new CrudGenerator();
         $this->config = [
-            'framework'                  => 'Laravel',
-            'bootstrap'                  => false,
-            'semantic'                   => false,
-            'template_source'            => __DIR__.'/../src/Templates/CRUD',
-            '_sectionPrefix_'            => '',
-            '_sectionTablePrefix_'       => '',
-            '_sectionRoutePrefix_'       => '',
-            '_sectionNamespace_'         => '',
-            'relationships'              => null,
-            'schema'                     => null,
-            '_path_facade_'              => vfsStream::url('Facades'),
-            '_path_service_'             => vfsStream::url('Services'),
-            '_path_model_'               => vfsStream::url('Models/'.ucfirst('testTable')),
-            '_path_controller_'          => vfsStream::url('Http/Controllers'),
-            '_path_api_controller_'      => vfsStream::url('Http/Controllers/Api'),
-            '_path_views_'               => vfsStream::url('resources/views'),
-            '_path_tests_'               => vfsStream::url('tests'),
-            '_path_request_'             => vfsStream::url('Http/Requests'),
-            '_path_routes_'              => vfsStream::url('Http/routes.php'),
-            '_path_api_routes_'          => vfsStream::url('Http/api-routes.php'),
-            '_path_factory_'             => vfsStream::url('database/factories/ModelFactory.php'),
-            'routes_prefix'              => '',
-            'routes_suffix'              => '',
-            '_namespace_services_'       => 'App\Services',
-            '_namespace_facade_'         => 'App\Facades',
-            '_namespace_repository_'     => 'App\Repositories\\'.ucfirst('testTable'),
-            '_namespace_model_'          => 'App\Repositories\\'.ucfirst('testTable'),
-            '_namespace_controller_'     => 'App\Http\Controllers',
+            'framework' => 'Laravel',
+            'bootstrap' => false,
+            'semantic' => false,
+            'template_source' => __DIR__.'/../src/Templates/CRUD',
+            '_sectionPrefix_' => '',
+            '_sectionTablePrefix_' => '',
+            '_sectionRoutePrefix_' => '',
+            '_sectionNamespace_' => '',
+            'relationships' => null,
+            'schema' => null,
+            '_path_facade_' => vfsStream::url('Facades'),
+            '_path_service_' => vfsStream::url('Services'),
+            '_path_model_' => vfsStream::url('Models/'.ucfirst('testTable')),
+            '_path_controller_' => vfsStream::url('Http/Controllers'),
+            '_path_api_controller_' => vfsStream::url('Http/Controllers/Api'),
+            '_path_views_' => vfsStream::url('resources/views'),
+            '_path_tests_' => vfsStream::url('tests'),
+            '_path_request_' => vfsStream::url('Http/Requests'),
+            '_path_routes_' => vfsStream::url('Http/routes.php'),
+            '_path_api_routes_' => vfsStream::url('Http/api-routes.php'),
+            '_path_factory_' => vfsStream::url('database/factories/ModelFactory.php'),
+            'routes_prefix' => '',
+            'routes_suffix' => '',
+            '_namespace_services_' => 'App\Services',
+            '_namespace_facade_' => 'App\Facades',
+            '_namespace_repository_' => 'App\Repositories\\'.ucfirst('testTable'),
+            '_namespace_model_' => 'App\Repositories\\'.ucfirst('testTable'),
+            '_namespace_controller_' => 'App\Http\Controllers',
             '_namespace_api_controller_' => 'App\Http\Controllers\Api',
-            '_namespace_request_'        => 'App\Http\Requests',
-            '_lower_case_'               => strtolower('testTable'),
-            '_lower_casePlural_'         => str_plural(strtolower('testTable')),
-            '_camel_case_'               => ucfirst(camel_case('testTable')),
-            '_camel_casePlural_'         => str_plural(camel_case('testTable')),
-            '_ucCamel_casePlural_'       => ucfirst(str_plural(camel_case('testTable'))),
+            '_namespace_request_' => 'App\Http\Requests',
+            '_lower_case_' => strtolower('testTable'),
+            '_lower_casePlural_' => str_plural(strtolower('testTable')),
+            '_camel_case_' => ucfirst(camel_case('testTable')),
+            '_camel_casePlural_' => str_plural(camel_case('testTable')),
+            '_ucCamel_casePlural_' => ucfirst(str_plural(camel_case('testTable'))),
         ];
     }
 
@@ -55,9 +55,9 @@ class CrudSingleGeneratorTest extends PHPUnit_Framework_TestCase
         $this->crud = vfsStream::setup('Http/Controllers');
         $this->generator->createController($this->config);
 
-        $this->assertTrue($this->crud->hasChild('Http/Controllers/TestTableController.php'));
         $contents = $this->crud->getChild('Http/Controllers/TestTableController.php');
 
+        $this->assertTrue($this->crud->hasChild('Http/Controllers/TestTableController.php'));
         $this->assertContains('class TestTableController extends Controller', $contents->getContent());
     }
 

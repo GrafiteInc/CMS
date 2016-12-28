1d42d76113c3ef8370d6d435895ced75b9546ce3
mlantz <mattlantz@gmail.com>
2016-12-28 17:05:17 -0500

    Better mobile support

--- src/PublishedAssets/Views/themes/default/layout/master.blade.php
+++ src/PublishedAssets/Views/themes/default/layout/master.blade.php
@@ -3,6 +3,8 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
         <title>Website</title>
         <meta name="description" content="">
         <meta name="author" content="">

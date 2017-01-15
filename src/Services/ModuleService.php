7c98d6b39340920823bfa121a47192dfde04a056
mlantz <mattlantz@gmail.com>
2017-01-15 03:31:49 -0500

    Updated for composer loaded packages

--- src/Services/ModuleService.php
+++ src/Services/ModuleService.php
@@ -2,14 +2,11 @@
 
 namespace Yab\Quarx\Services;
 
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\View;
-
 class ModuleService
 {
     public function menus()
     {
-        $modulePath = base_path(Config::get('quarx.module-directory').'/');
+        $modulePath = base_path(config('quarx.module-directory').'/');
         $modules = glob($modulePath.'*');
 
         $menu = '';
@@ -18,7 +15,15 @@ class ModuleService
             if (is_dir($module)) {
                 $module = lcfirst(str_replace($modulePath, '', $module));
                 if (file_exists($modulePath.ucfirst($module).'/Views/menu.blade.php')) {
-                    $menu .= View::make($module.'::menu');
+                    $menu .= view($module.'::menu');
+                }
+            }
+        }
+
+        if (is_array(config('quarx.modules'))) {
+            foreach (config('quarx.modules') as $module => $config) {
+                if (!is_dir($modulePath.$module)) {
+                    $menu .= view($module.'::menu');
                 }
             }
         }

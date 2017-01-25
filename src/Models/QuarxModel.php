449e6c1e981f56f3f42258ccd9fb48a0aac98a59
mlantz <mattlantz@gmail.com>
2017-01-25 00:18:07 -0500

    Prevent archiving when reverting or rolling back

--- src/Models/QuarxModel.php
+++ src/Models/QuarxModel.php
@@ -28,19 +28,21 @@ class QuarxModel extends Model
      */
     public function afterSaved($payload)
     {
-        unset($payload->attributes['created_at']);
-        unset($payload->attributes['updated_at']);
-        unset($payload->original['created_at']);
-        unset($payload->original['updated_at']);
+        if (!request()->is('quarx/revert/*') && !request()->is('quarx/rollback/*/*')) {
+            unset($payload->attributes['created_at']);
+            unset($payload->attributes['updated_at']);
+            unset($payload->original['created_at']);
+            unset($payload->original['updated_at']);
 
-        if ($payload->attributes != $payload->original) {
-            Archive::create([
-                'token' => md5(time()),
-                'entity_id' => $payload->attributes['id'],
-                'entity_type' => get_class($payload),
-                'entity_data' => json_encode($payload->attributes),
-            ]);
-            Log::info(get_class($payload).' #'.$payload->attributes['id'].' was archived');
+            if ($payload->attributes != $payload->original) {
+                Archive::create([
+                    'token' => md5(time()),
+                    'entity_id' => $payload->attributes['id'],
+                    'entity_type' => get_class($payload),
+                    'entity_data' => json_encode($payload->attributes),
+                ]);
+                Log::info(get_class($payload).' #'.$payload->attributes['id'].' was archived');
+            }
         }
     }
 

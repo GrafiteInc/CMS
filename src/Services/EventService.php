80655f27248603f73696e474fd44fa22712b674e
imanghafoori <imanghafoori1@gmail.com>
2017-09-10 00:12:46 +0430

    Simpler code

--- src/Services/EventService.php
+++ src/Services/EventService.php
@@ -148,11 +148,12 @@ class EventService
 
         foreach ($templates as $template) {
             $template = str_replace(base_path('resources/themes/'.Config::get('quarx.frontend-theme').'/events/'), '', $template);
-            if (stristr($template, 'template')) {
-                $template = str_replace('-template.blade.php', '', $template);
-                if (!stristr($template, '.php')) {
-                    $availableTemplates[] = $template.'-template';
-                }
+            if (!stristr($template, 'template')) {
+                continue;
+            }
+            $template = str_replace('-template.blade.php', '', $template);
+            if (!stristr($template, '.php')) {
+                $availableTemplates[] = $template.'-template';
             }
         }
 

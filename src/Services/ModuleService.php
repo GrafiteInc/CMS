32d854f40dfbfa204b24da45f7836c05cc5150eb
imanghafoori <imanghafoori1@gmail.com>
2017-09-10 00:15:34 +0430

    Simpler code

--- src/Services/ModuleService.php
+++ src/Services/ModuleService.php
@@ -7,27 +7,38 @@ class ModuleService
     public function menus()
     {
         $modulePath = base_path(config('quarx.module-directory').'/');
-        $modules = glob($modulePath.'*');
 
+        $menu = $this->_getMenuViews($modulePath);
+
+        if (!is_array(config('quarx.modules'))) {
+            return $menu;
+        }
+
+        foreach (config('quarx.modules') as $module => $config) {
+            if (!is_dir($modulePath.ucfirst($module))) {
+                $menu .= view($module.'::menu');
+            }
+        }
+
+        return $menu;
+    }
+
+    /**
+     * @param $modulePath
+     * @return array
+     */
+    private function _getMenuViews($modulePath)
+    {
         $menu = '';
-
-        foreach ($modules as $module) {
-            if (is_dir($module)) {
-                $module = lcfirst(str_replace($modulePath, '', $module));
-                if (file_exists($modulePath.ucfirst($module).'/Views/menu.blade.php')) {
-                    $menu .= view($module.'::menu');
-                }
+        foreach (glob($modulePath.'*') as $module) {
+            if (!is_dir($module)) {
+                continue;
+            }
+            $module = lcfirst(str_replace($modulePath, '', $module));
+            if (file_exists($modulePath . ucfirst($module) . '/Views/menu.blade.php')) {
+                $menu .= view($module . '::menu');
             }
         }
-
-        if (is_array(config('quarx.modules'))) {
-            foreach (config('quarx.modules') as $module => $config) {
-                if (!is_dir($modulePath.ucfirst($module))) {
-                    $menu .= view($module.'::menu');
-                }
-            }
-        }
-
         return $menu;
     }
 }

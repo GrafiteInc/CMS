bac806c368b0f34b7a95c2b562610ab298914e16
Matt Lantz <mattlantz@gmail.com>
2017-09-17 16:34:16 -0400

    Enhanced admin dashboard

--- src/PublishedAssets/Setup/resources/views/admin/dashboard.blade.php
+++ src/PublishedAssets/Setup/resources/views/admin/dashboard.blade.php
@@ -9,7 +9,66 @@
         <h1 class="page-header">Admin Dashboard</h1>
     </div>
     <div class="row">
-
+        <div class="col-lg-2">
+            <div class="panel panel-default text-center">
+                <div class="panel-heading">
+                    Users
+                </div>
+                <div class="panel-body">
+                    <span class="lead">{{ app(App\Models\User::class)->count() }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="panel panel-default text-center">
+                <div class="panel-heading">
+                    Blogs
+                </div>
+                <div class="panel-body">
+                    <span class="lead">{{ app(Yab\Quarx\Models\Blog::class)->count() }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="panel panel-default text-center">
+                <div class="panel-heading">
+                    Pages
+                </div>
+                <div class="panel-body">
+                    <span class="lead">{{ app(Yab\Quarx\Models\Page::class)->count() }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="panel panel-default text-center">
+                <div class="panel-heading">
+                    Widgets
+                </div>
+                <div class="panel-body">
+                    <span class="lead">{{ app(Yab\Quarx\Models\Widget::class)->count() }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="panel panel-default text-center">
+                <div class="panel-heading">
+                    Events
+                </div>
+                <div class="panel-body">
+                    <span class="lead">{{ app(Yab\Quarx\Models\Event::class)->count() }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="panel panel-default text-center">
+                <div class="panel-heading">
+                    FAQs
+                </div>
+                <div class="panel-body">
+                    <span class="lead">{{ app(Yab\Quarx\Models\FAQ::class)->count() }}</span>
+                </div>
+            </div>
+        </div>
     </div>
 
 @stop

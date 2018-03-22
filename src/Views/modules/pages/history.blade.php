d6bf3f1e3bfb6adb04a00c641329eba45500cba9
Matt Lantz <mattlantz@gmail.com>
2018-03-22 10:54:40 -0400

    UI clean  up

--- src/Views/modules/pages/history.blade.php
+++ src/Views/modules/pages/history.blade.php
@@ -4,21 +4,21 @@
 
 @section('content')
 
-    @include('cms::modules.pages.breadcrumbs', ['location' => [[$page->title => cms()->url('pages/'.$page->id.'/edit')], 'history']])
+    <div class="col-md-12 mt-2">
+        @include('cms::modules.pages.breadcrumbs', ['location' => [[$page->title => cms()->url('pages/'.$page->id.'/edit')], 'history']])
+    </div>
 
-    <div class="row mt-4">
-        <div class="col-md-12">
-            <table class="table table-striped">
-                @foreach($page->history() as $history)
-                    <tr>
-                        <td>{{ $history->created_at->format('M jS, Y') }} ({{ $history->created_at->diffForHumans() }})</td>
-                        <td class="text-right">
-                            <a class="btn btn-outline-warning btn-sm" href="{{ cms()->url('revert/'.$history->id) }}">Revert</a>
-                        </td>
-                    </tr>
-                @endforeach
-            </table>
-        </div>
+    <div class="col-md-12 mt-4">
+        <table class="table table-striped">
+            @foreach($page->history() as $history)
+                <tr>
+                    <td>{{ $history->created_at->format('M jS, Y') }} ({{ $history->created_at->diffForHumans() }})</td>
+                    <td class="text-right">
+                        <a class="btn btn-outline-warning btn-sm" href="{{ cms()->url('revert/'.$history->id) }}">Revert</a>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
     </div>
 
 @endsection

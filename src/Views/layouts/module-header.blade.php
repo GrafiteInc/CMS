9abcb987f03f57a9b16ca816f2bc432b2062ab0e
Matt Lantz <mattlantz@gmail.com>
2018-04-03 08:47:10 -0400

    Route fixer

--- src/Views/layouts/module-header.blade.php
+++ src/Views/layouts/module-header.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-12">
     <nav class="navbar px-0 navbar-light justify-content-between">
         <ul class="navbar-nav">
-            @if (Route::has('cms.'.$module.'.create'))
+            @if (Route::has(cms()->route($module.'.create')))
                 <li class="nav-item">
                     <a class="btn btn-primary" href="{!! route(cms()->route($module.'.create')) !!}">Add New</a>
                 </li>

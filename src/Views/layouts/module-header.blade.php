f352d82fba27c5c2c710e82cfddf436a85fa5244
Matt Lantz <mattlantz@gmail.com>
2018-04-03 00:58:17 -0400

    popper and route fix

--- src/Views/layouts/module-header.blade.php
+++ src/Views/layouts/module-header.blade.php
@@ -1,9 +1,11 @@
 <div class="col-md-12">
     <nav class="navbar px-0 navbar-light justify-content-between">
         <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="btn btn-primary" href="{!! route(cms()->route($module.'.create')) !!}">Add New</a>
-            </li>
+            @if (Route::has('cms.'.$module.'.create'))
+                <li class="nav-item">
+                    <a class="btn btn-primary" href="{!! route(cms()->route($module.'.create')) !!}">Add New</a>
+                </li>
+            @endif
         </ul>
         {!! Form::open(['url' => cms()->url($module.'/search'), 'class' => 'form-inline mt-2']) !!}
             <input class="form-control mr-sm-2" name="term" type="search" placeholder="Search" aria-label="Search">

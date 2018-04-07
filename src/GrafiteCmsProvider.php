69753024d9d78193cd24ccf2b096e07523594e69
Simon <Simon@Ludwigs.org>
2018-04-07 16:33:07 +0200

    Exchange LaravelAnalyticsFacade to AnalyticsFacade, but keep internal alias.

--- src/GrafiteCmsProvider.php
+++ src/GrafiteCmsProvider.php
@@ -15,10 +15,8 @@ use Illuminate\Support\Str;
 use Intervention\Image\Facades\Image;
 use Intervention\Image\ImageServiceProvider;
 use Cms;
-//use Spatie\LaravelAnalytics\LaravelAnalyticsFacade;
-use Spatie\Analytics\AnalyticsFacade;
-//use Spatie\LaravelAnalytics\LaravelAnalyticsServiceProvider;
-use Spatie\Analytics\AnalyticsServiceProvider
+use Spatie\Analytics\AnalyticsFacade; //use Spatie\LaravelAnalytics\LaravelAnalyticsFacade;
+use Spatie\Analytics\AnalyticsServiceProvider; //use Spatie\LaravelAnalytics\LaravelAnalyticsServiceProvider;
 use Grafite\Builder\GrafiteBuilderProvider;
 use Grafite\Cms\Console\Keys;
 use Grafite\Cms\Console\ModuleComposer;
